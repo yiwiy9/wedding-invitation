f7ab2bdaa0738797dfe5934a477a29bcc2fa497f
yiwiy9 <yiwiy9@gmail.com>
2022-10-31 18:14:46 +0900

    Add .devcontainer

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 秘密情報をビルドに含めない
+# serverless framework にAWSアカウントを紐付ける
+serverless config credentials \
+    --provider aws \
+    --key $AWS_ACCESS_KEY_ID \
+    --secret $AWS_SECRET_ACCESS_KEY &&
+
+# 依存パッケージのインストール
+yarn &&
+
+# exitしないためにベースイメージの`CMD`実行
+node
