d71a506d50da65a660d82007c40b9eb94e1aa48b
fumiya-iwai <yyffii0912@gmail.com>
2022-11-28 03:49:33 +0900

    learn tailwind

---
+++ frontend/src/practice.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Practice() {
+  return (
+    <div>
+      <h1 className='bg-red-500 text-black text-2xl text-center'>iwai fumiya</h1>
+    </div>
+  )
+}
+
+export default Practice
