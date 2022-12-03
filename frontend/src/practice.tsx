822bca7791ccca328621ea473e62814945e91804
fumiya-iwai <yyffii0912@gmail.com>
2022-12-04 00:33:13 +0900

    難しい

--- frontend/src/practice.tsx
+++ frontend/src/practice.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import weddingImage from './wedding_image.webp'
 
 function Practice() {
+  const styles = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+  })
   return (
     <div>
-      <h1 className='bg-red-500 text-black text-2xl text-center'>iwai fumiya</h1>
+      <img src={weddingImage} alt='wedding' className='w-full' />
+      <animated.div style={styles} className='absolute top-1/2 left-1/2 -translate-x-1/2 transform'>
+        <h1 className='animate-pulse font-serif text-6xl text-white'>Welcome to our wedding</h1>
+      </animated.div>
     </div>
   )
 }
