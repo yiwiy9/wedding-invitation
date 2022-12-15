fb5941f6b84104266e4ba129abedbab751c11ddc
fumiya-iwai <yyffii0912@gmail.com>
2022-12-16 02:38:04 +0900

    react-hook-form

--- frontend/src/practice.tsx
+++ frontend/src/practice.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 function Form() {
   const [name, setName] = useState('')
@@ -22,7 +23,6 @@ function Form() {
   return (
     <div>
       <p>Name</p>
-      {error && <span className='text-red-600'>{error}</span>}
       <p />
       <input
         type='text'
@@ -30,6 +30,7 @@ function Form() {
         onChange={handleNameChange}
         className='border-2 border-solid border-black'
       />
+      {error && <span className='text-red-600'>{error}</span>}
       <p />
       <button
         onClick={handleSubmit}
@@ -42,11 +43,47 @@ function Form() {
   )
 }
 
+function HookForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = (data: object) => {
+    console.log(data)
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p>Name</p>
+      <input
+        {...register('name', { required: true })}
+        className='border-2 border-solid border-black'
+      />
+      {errors.name && <span className='text-red-600'>必須項目です</span>}
+      <p>Email</p>
+      <input
+        className='border-2 border-solid border-black'
+        {...register('email', {
+          required: true,
+          minLength: 5,
+        })}
+      />
+      {errors.email && <span className='text-red-600'>５文字以上の入力が必要です</span>}
+      <p></p>
+      <button type='submit' className='rounded bg-indigo-700 py-2 px-4 font-semibold text-white'>
+        Submit
+      </button>
+    </form>
+  )
+}
+
 function Practice() {
   return (
     <div>
       <h1 className='text-5xl'>welcome to our wedding</h1>
       <Form />
+      <p></p>
+      <HookForm />
     </div>
   )
 }
