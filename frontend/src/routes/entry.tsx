5a56a905386d770919911a1577d881d03b4cdf3d
fumiya-iwai <yyffii0912@gmail.com>
2022-12-28 09:50:25 +0900

    add children1

--- frontend/src/routes/entry.tsx
+++ frontend/src/routes/entry.tsx
@@ -13,6 +13,7 @@ const Entry: FC = () => {
     reValidateMode: 'onChange',
   })
   const [show, setShow] = useState(false)
+  const [childNum, setChildNum] = useState([...Array(0)])
   const onSubmit = (data: object) => {
     console.log(data)
     setShow(true)
@@ -23,112 +24,183 @@ const Entry: FC = () => {
       <div className='absolute top-40 left-1/2 w-1/2 -translate-x-1/2'>
         <div className='font-serif'>
           <h1 className='pb-10 text-center text-8xl'>R.V.S.P</h1>
-          <form onSubmit={handleSubmit(onSubmit)} className='text-xl '>
-            <div className='text-center'>
-              <input
-                value='attend'
-                type='radio'
-                {...register('attend', {
-                  required: { value: true, message: '出席・欠席を選んでください' },
-                })}
-              />
-              <span className='pr-5 pl-3'>will attend</span>
-              <input value='absent' type='radio' {...register('attend')} />
-              <span className='pl-3'>will not attend</span>
-              {errors?.attend?.message ? (
-                <span className='block text-center text-red-600'>
-                  {String(errors.attend.message)}
-                </span>
-              ) : (
-                <div>
-                  <br />
-                </div>
-              )}
-            </div>
-            <div className='pt-5'>
-              <label className='inline-block w-60 text-right'>
-                Name<sup>*</sup>
-              </label>
-              <input
-                {...register('name', {
-                  required: { value: true, message: '名前は入力必須項目です' },
-                })}
-                className='ml-10 inline-block border-2 border-solid border-black'
-              />
-              <br />
-              {errors?.name?.message ? (
-                <span className='block text-center text-red-600'>
-                  {String(errors.name.message)}
-                </span>
-              ) : (
-                <br />
-              )}
-              <label className='inline-block w-60 text-right'>Adress</label>
-              <input
-                {...register('adress')}
-                className='ml-10 inline-block border-2 border-solid border-black'
-              />
-              <br />
-              <br />
-              <label className='inline-block w-60 text-right'>Phone number</label>
-              <input
-                {...register('phoneNumber')}
-                className='ml-10 inline-block border-2 border-solid border-black'
-              />
-              <br />
-              <br />
-              <label className='inline-block w-60 text-right'>
-                Email<sup>*</sup>
-              </label>
-              <input
-                className='ml-10 inline-block border-2 border-solid border-black'
-                {...register('email', {
-                  required: { value: true, message: 'メールアドレスは入力必須項目です' },
-                  pattern: {
-                    value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                    message: '有効なメールアドレスを入力してください',
-                  },
-                })}
-              />
-              <br />
-              {errors?.email?.message ? (
-                <span className='block text-center text-red-600'>
-                  {String(errors.email.message)}
-                </span>
-              ) : (
-                <br />
-              )}
-
-              <label className='inline-block w-60 text-right'>Allergy</label>
-              <input
-                {...register('allergy')}
-                className='ml-10 inline-block border-2 border-solid border-black'
-              />
-              <br />
-              <br />
-              <label className='inline-block w-60 text-right'>Message</label>
-              <textarea
-                {...register('message')}
-                className='ml-10 inline-block border-2 border-solid border-black align-top'
-              />
-              <br />
-              <br />
-              <button
-                type='submit'
-                disabled={!isDirty}
-                className={
-                  isDirty
-                    ? 'm-auto block rounded bg-gray-600 px-4 py-2 text-white hover:bg-gray-500'
-                    : 'm-auto block rounded bg-gray-300 px-4 py-2 text-white'
-                }
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          <MainForm
+            register={register}
+            errors={errors}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit}
+            isDirty={isDirty}
+            childNum={childNum}
+            setChildNum={setChildNum}
+          />
         </div>
       </div>
       {show ? <Modal modalHidden={() => setShow(false)} /> : null}
+    </div>
+  )
+}
+
+const MainForm: FC<{
+  register: any
+  errors: any
+  handleSubmit: any
+  onSubmit: any
+  isDirty: any
+  childNum: any
+  setChildNum: any
+}> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit(props.onSubmit)} className='text-xl '>
+      <div className='text-center'>
+        <input
+          value='attend'
+          type='radio'
+          {...props.register('attend', {
+            required: { value: true, message: '出席・欠席を選んでください' },
+          })}
+        />
+        <span className='pr-5 pl-3'>attend</span>
+        <input value='absent' type='radio' {...props.register('attend')} />
+        <span className='pl-3'>decline</span>
+        {props.errors?.attend?.message ? (
+          <span className='block text-center text-red-600'>
+            {String(props.errors.attend.message)}
+          </span>
+        ) : (
+          <div>
+            <br />
+          </div>
+        )}
+      </div>
+      <div className='pt-5'>
+        <label className='inline-block w-60 text-right'>
+          Name<sup>*</sup>
+        </label>
+        <input
+          {...props.register('name', {
+            required: { value: true, message: '名前は入力必須項目です' },
+          })}
+          className='ml-10 inline-block border-2 border-solid border-black'
+        />
+        <br />
+        {props.errors?.name?.message ? (
+          <span className='block text-center text-red-600'>
+            {String(props.errors.name.message)}
+          </span>
+        ) : (
+          <br />
+        )}
+        <label className='inline-block w-60 text-right'>Adress</label>
+        <input
+          {...props.register('adress')}
+          className='ml-10 inline-block border-2 border-solid border-black'
+        />
+        <br />
+        <br />
+        <label className='inline-block w-60 text-right'>Phone number</label>
+        <input
+          {...props.register('phoneNumber')}
+          className='ml-10 inline-block border-2 border-solid border-black'
+        />
+        <br />
+        <br />
+        <label className='inline-block w-60 text-right'>
+          Email<sup>*</sup>
+        </label>
+        <input
+          className='ml-10 inline-block border-2 border-solid border-black'
+          {...props.register('email', {
+            required: { value: true, message: 'メールアドレスは入力必須項目です' },
+            pattern: {
+              value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: '有効なメールアドレスを入力してください',
+            },
+          })}
+        />
+        <br />
+        {props.errors?.email?.message ? (
+          <span className='block text-center text-red-600'>
+            {String(props.errors.email.message)}
+          </span>
+        ) : (
+          <br />
+        )}
+
+        <label className='inline-block w-60 text-right'>Allergy</label>
+        <input
+          {...props.register('allergy')}
+          className='ml-10 inline-block border-2 border-solid border-black'
+        />
+        <br />
+        <br />
+        <label className='inline-block w-60 text-right'>Message</label>
+        <textarea
+          {...props.register('message')}
+          className='ml-10 inline-block border-2 border-solid border-black align-top'
+        />
+        <br />
+        <br />
+        {props.childNum.length === 0
+          ? null
+          : props.childNum.map((value: any, index: any) => {
+              return <ChildForm register={props.register} errors={props.errors} num={index + 1} />
+            })}
+        <br />
+        <br />
+        <button
+          type='button'
+          onClick={() => {
+            props.setChildNum([...Array(props.childNum.length + 1)])
+          }}
+          className='ml-auto block rounded border-2 border-solid border-black px-4 py-2'
+        >
+          + Add children
+        </button>
+        <button
+          type='submit'
+          disabled={!props.isDirty}
+          className={
+            props.isDirty
+              ? 'm-auto block rounded bg-gray-600 px-4 py-2 text-white hover:bg-gray-500'
+              : 'm-auto block rounded bg-gray-300 px-4 py-2 text-white'
+          }
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const ChildForm: FC<{ register: any; errors: any; num: any }> = (props) => {
+  return (
+    <div>
+      <div className='text-center'>children {props.num}</div>
+      <br></br>
+      <label className='inline-block w-60 text-right'>
+        Name<sup>*</sup>
+      </label>
+      <input
+        {...props.register(`childName${props.num}`, {
+          required: { value: true, message: '名前は入力必須項目です' },
+        })}
+        className='ml-10 inline-block border-2 border-solid border-black'
+      />
+      <br />
+      {props.errors?.childName?.message ? (
+        <span className='block text-center text-red-600'>
+          {String(props.errors.childName.message)}
+        </span>
+      ) : (
+        <br />
+      )}
+      <label className='inline-block w-60 text-right'>Allergy</label>
+      <input
+        {...props.register(`childAllergy${props.num}`)}
+        className='ml-10 inline-block border-2 border-solid border-black'
+      />
+      <br />
+      <br />
     </div>
   )
 }
