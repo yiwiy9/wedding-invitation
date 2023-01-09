a03ff239aa46d145bf6cb176758d44d16da04da0
fumiya-iwai <yyffii0912@gmail.com>
2023-01-09 23:49:57 +0900

    a

--- frontend/src/routes/entry.tsx
+++ frontend/src/routes/entry.tsx
@@ -8,6 +8,7 @@ import {
   UseFormHandleSubmit,
   FieldValues,
 } from 'react-hook-form'
+import axios from 'axios'
 
 type MainProps = {
   register: UseFormRegister<FieldValues>
@@ -15,7 +16,6 @@ type MainProps = {
   errors: Partial<FieldErrorsImpl<{ [x: string]: string }>>
   handleSubmit: UseFormHandleSubmit<FieldValues>
   onSubmit: (data: object) => void
-  isDirty: boolean
   childrenArray: Array<number>
   setChildrenArray: Dispatch<SetStateAction<Array<number>>>
   addNum: number
@@ -45,40 +45,43 @@ const ChildForm: FC<ChildProps> = (props) => {
       setAddNum(0)
     }
     setChildrenArray(array)
-    unregister([`childAllergy${childNum}`, childName])
+    unregister([childAllergy, childName])
   }
 
   return (
-    <div>
+    <div className='mt-10'>
       <div className='text-center'>子供 {childNum}</div>
       <br />
-      <span className='inline-block w-60 text-right'>
+      <span className='block text-sm'>
         名前<sup>*</sup>
       </span>
+      {errors?.[childName]?.message ? (
+        <span className='text-red-600'>{errors?.[childName]?.message}</span>
+      ) : null}
       <input
         {...register(childName, {
           required: { value: true, message: '名前は入力必須項目です' },
         })}
-        className='ml-10 inline-block border-2 border-solid border-black'
+        type='text'
+        placeholder='山田花子'
+        className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
       />
       <br />
-      {errors?.[childName]?.message ? (
-        <span className='block text-center text-red-600'>{errors?.[childName]?.message}</span>
-      ) : (
-        <br />
-      )}
-      <span className='inline-block w-60 text-right'>アレルギー</span>
+      <br />
+      <span className='block text-sm'>アレルギー</span>
       <input
         {...register(childAllergy)}
-        className='ml-10 inline-block border-2 border-solid border-black'
+        type='text'
+        placeholder='そば'
+        className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
       />
       <br />
       <button
         type='button'
         onClick={deleteChild}
-        className='ml-auto block w-1/2 rounded px-4 py-2 underline'
+        className='ml-auto block w-1/4 rounded py-5 underline'
       >
-        delete
+        削除
       </button>
     </div>
   )
@@ -91,7 +94,6 @@ const MainForm: FC<MainProps> = (props) => {
     errors,
     handleSubmit,
     onSubmit,
-    isDirty,
     childrenArray,
     setChildrenArray,
     addNum,
@@ -106,8 +108,27 @@ const MainForm: FC<MainProps> = (props) => {
     setAddNum(num)
   }
 
+  const [fullAdress, setFullAdress] = useState('')
+  const [zipCode, setZipCode] = useState('')
+
+  type Res = {
+    data: { code: number; data: { fullAdress: string } }
+  }
+
+  const handleChangeAdress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFullAdress(e.target.value)
+  }
+
+  const handleChangeZipCode = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setZipCode(e.target.value)
+    const res: Res = await axios.get(`https://api.zipaddress.net/?zipcode=${e.target.value}`)
+    if (res.data.code === 200) {
+      setFullAdress(res.data.data.fullAdress)
+    }
+  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='text-xl '>
+    <form onSubmit={handleSubmit(onSubmit)} className='mx-auto md:max-w-md'>
       <div className='text-center'>
         <input
           type='radio'
@@ -116,21 +137,22 @@ const MainForm: FC<MainProps> = (props) => {
             required: { value: true, message: '出席・欠席を選んでください' },
           })}
         />
-        <span className='pr-5 pl-3'>出席</span>
+        <span className='pr-10 pl-3'>出席</span>
         <input type='radio' value='false' {...register('isAttend')} />
         <span className='pl-3'>欠席</span>
         {errors?.isAttend?.message ? (
           <span className='block text-center text-red-600'>{errors.isAttend.message}</span>
-        ) : (
-          <div>
-            <br />
-          </div>
-        )}
+        ) : null}
+        <br />
+        <br />
       </div>
       <div className='pt-5'>
-        <span className='inline-block w-60 text-right'>
+        <span className='block text-sm'>
           姓（漢字）<sup>*</sup>
         </span>
+        {errors?.familyNameKanji?.message ? (
+          <span className='text-red-600'>{errors.familyNameKanji.message}</span>
+        ) : null}
         <input
           {...register('familyNameKanji', {
             required: { value: true, message: '姓（漢字）は入力必須項目です' },
@@ -138,34 +160,32 @@ const MainForm: FC<MainProps> = (props) => {
           type='text'
           autoComplete='family-name'
           placeholder='山田'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
-        {errors?.familyNameKanji?.message ? (
-          <span className='block text-center text-red-600'>{errors.familyNameKanji.message}</span>
-        ) : (
-          <br />
-        )}
-        <span className='inline-block w-60 text-right'>
+        <br />
+        <span className='block text-sm'>
           姓（かな）<sup>*</sup>
         </span>
+        {errors?.familyNameKana?.message ? (
+          <span className='text-red-600'>{errors.familyNameKana.message}</span>
+        ) : null}
         <input
           {...register('familyNameKana', {
             required: { value: true, message: '姓（かな）は入力必須項目です' },
           })}
           type='text'
           placeholder='やまだ'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
-        {errors?.familyNameKana?.message ? (
-          <span className='block text-center text-red-600'>{errors.familyNameKana.message}</span>
-        ) : (
-          <br />
-        )}
-        <span className='inline-block w-60 text-right'>
+        <br />
+        <span className='block text-sm'>
           名（漢字）<sup>*</sup>
         </span>
+        {errors?.givenNameKanji?.message ? (
+          <span className='text-red-600'>{errors.givenNameKanji.message}</span>
+        ) : null}
         <input
           {...register('givenNameKanji', {
             required: { value: true, message: '名（漢字）は入力必須項目です' },
@@ -173,95 +193,94 @@ const MainForm: FC<MainProps> = (props) => {
           type='text'
           autoComplete='given-name'
           placeholder='太郎'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
-        {errors?.givenNameKanji?.message ? (
-          <span className='block text-center text-red-600'>{errors.givenNameKanji.message}</span>
-        ) : (
-          <br />
-        )}
-        <span className='inline-block w-60 text-right'>
+        <br />
+        <span className='block text-sm'>
           名（かな）<sup>*</sup>
         </span>
+        {errors?.givenNameKana?.message ? (
+          <span className='text-red-600'>{errors.givenNameKana.message}</span>
+        ) : null}
         <input
           {...register('givenNameKana', {
             required: { value: true, message: '名（かな）は入力必須項目です' },
           })}
           type='text'
           placeholder='たろう'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
-        {errors?.givenNameKana?.message ? (
-          <span className='block text-center text-red-600'>{errors.givenNameKana.message}</span>
-        ) : (
-          <br />
-        )}
-        <span className='inline-block w-60 text-right'>郵便番号</span>
+        <br />
+        <span className='block text-sm'>郵便番号</span>
         <input
           {...register('zipCode')}
           type='text'
+          onChange={handleChangeZipCode}
+          value={zipCode}
           autoComplete='postal-code'
           placeholder='1234567'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>住所１</span>
+        <span className='block text-sm'>住所</span>
         <input
           {...register('adress1')}
+          onChange={handleChangeAdress}
+          value={fullAdress}
           type='text'
           autoComplete='address-line1'
           placeholder='東京都ｘｘ区ｘｘｘ 1-1-1'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>住所２</span>
+        <span className='block text-sm'>建物名・部屋番号等</span>
         <input
           {...register('adress2')}
           type='text'
           autoComplete='address-line2'
           placeholder='ｘｘマンション 101号室'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>電話番号</span>
+        <span className='block text-sm'>電話番号</span>
         <input
           {...register('tel')}
           type='tel'
           autoComplete='tel'
           placeholder='09012345678'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>メールアドレス</span>
+        <span className='block text-sm'>メールアドレス</span>
         <input
           {...register('email')}
           type='email'
           autoComplete='email'
           placeholder='yamada@example.com'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>アレルギー</span>
+        <span className='block text-sm'>アレルギー</span>
         <input
           {...register('allergy')}
           type='text'
           placeholder='卵'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
-        <span className='inline-block w-60 text-right'>メッセージ</span>
+        <span className='block text-sm'>メッセージ</span>
         <textarea
           {...register('message')}
           placeholder='おめでとう！'
-          className='ml-10 inline-block w-1/2 border-2 border-solid border-black p-1 align-top'
+          className='w-full border-b py-2 placeholder-gray-500 placeholder-opacity-50 focus:border-b-2 focus:border-indigo-500 focus:outline-none'
         />
         <br />
         <br />
@@ -282,30 +301,16 @@ const MainForm: FC<MainProps> = (props) => {
         <button
           type='button'
           onClick={addChildren}
-          className='m-auto block rounded border-2 border-solid border-black px-4 py-2'
+          className='m-auto block rounded px-4 py-2 underline'
         >
-          + Add children
+          + 子供を追加する
         </button>
         <br />
         <button
-          type='button'
-          className='ml-auto block w-1/2 rounded px-4 py-2 underline'
-          onClick={() => {
-            window.location.reload()
-          }}
-        >
-          reset all
-        </button>
-        <button
           type='submit'
-          disabled={!isDirty}
-          className={
-            isDirty
-              ? 'm-auto block rounded bg-gray-600 px-4 py-2 text-white hover:bg-gray-500'
-              : 'm-auto block rounded bg-gray-300 px-4 py-2 text-white'
-          }
+          className='m-auto my-5 block rounded bg-blue-700 px-4 py-2 text-white hover:bg-blue-600'
         >
-          Submit
+          送信
         </button>
       </div>
     </form>
@@ -335,7 +340,7 @@ const Entry: FC = () => {
     register,
     unregister,
     handleSubmit,
-    formState: { errors, isDirty },
+    formState: { errors },
   } = useForm()
   const [show, setShow] = useState(false)
   const [childrenArray, setChildrenArray] = useState(new Array(0))
@@ -347,14 +352,13 @@ const Entry: FC = () => {
 
   return (
     <div className='relative'>
-      <div className='absolute top-20 left-1/2 w-3/5 -translate-x-1/2'>
-        <h1 className='pb-10 text-center font-serif text-8xl'>R.V.S.P</h1>
+      <div className='absolute top-20 left-1/2 w-3/5 -translate-x-1/2 pb-20'>
+        <h1 className='pb-10 text-center font-serif text-6xl'>招待状</h1>
         <MainForm
           register={register}
           errors={errors}
           handleSubmit={handleSubmit}
           onSubmit={onSubmit}
-          isDirty={isDirty}
           childrenArray={childrenArray}
           setChildrenArray={setChildrenArray}
           addNum={addNum}
